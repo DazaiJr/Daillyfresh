10fcdd53c3aa31ab3db9160272985144460e5431
DazaiJr <ykshirsagar554@gmail.com>
2026-01-26 18:15:08 +0530

    Add login and register page

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('home/', home, name='home_redirect'),
+    path('login/',login,name='login'),
+    path('register/',register,name='register'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
