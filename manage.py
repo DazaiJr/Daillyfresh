2c00ae4788d5200a1969204da23ef42fbd563e07
DazaiJr <ykshirsagar554@gmail.com>
2026-02-01 16:21:31 +0530

    update git workflow

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    # CHANGE: 'config.settings' -> 'config.settings.local'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
